9732d1d6a635bfe2d416dd845548cb3cb2035990
Cyril Kuzmin <cyrilkuzmin@localhost.localdomain>
2020-09-10 19:53:05 +0300

    prot 0

--- app/go.mod
+++ app/go.mod
@@ -2,4 +2,8 @@ module github.com/xxlaefxx/shortlinks
 
 go 1.14
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/gorilla/mux v1.8.0
+	github.com/sirupsen/logrus v1.6.0
+)
--- app/main.go
+++ app/main.go
@@ -2,11 +2,30 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
+	"net/url"
+	"os"
+	"strings"
 
 	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
 )
+
+var domainName = "cyrilit.tk"
+var log = logrus.New()
+
+func makeLinkChecks(URL string) error {
+	// if it's not a URL at all
+	_, err := url.ParseRequestURI(URL)
+	if err != nil {
+		return err
+	}
+	// if it's already our URL
+	if strings.HasPrefix(URL, "https://"+domainName) {
+		return fmt.Errorf("It's already short")
+	}
+	return nil
+}
 
 func getLink(w http.ResponseWriter, req *http.Request) {
 	fmt.Println(req.URL)
@@ -16,17 +35,32 @@ func getShort(w http.ResponseWriter, req *http.Request) {
 	keys, ok := req.URL.Query()["link"]
 
 	if !ok || len(keys[0]) < 1 {
-		log.Println("Url Param 'link' is missing")
+		log.Error("Param 'link' is missing")
+		w.WriteHeader(400)
+		fmt.Fprintln(w, "Param 'link' is missing")
 		return
 	}
 
 	link := keys[0]
-	fmt.Println(link)
+
+	err := makeLinkChecks(link)
+
+	if err != nil {
+		log.Error(err)
+		w.WriteHeader(400)
+		fmt.Fprintln(w, err.Error())
+		return
+	}
 
 	w.Write([]byte("https://cyrilit.tk/s/Qwe21"))
 }
 
 func main() {
+	log.Formatter = &logrus.TextFormatter{
+		FullTimestamp: true,
+	}
+	log.SetOutput(os.Stdout)
+
 	rtr := mux.NewRouter()
 	rtr.HandleFunc("/getlink/{*}", getLink).Methods("GET")
 	rtr.HandleFunc("/getshort", getShort).Methods("GET")
