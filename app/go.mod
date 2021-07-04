668ba1aa86f52006b9c9d61ff852a0cd73e4f300
Laef Probel <xxlaefxx@gmail.com>
2021-07-04 19:20:00 +0300

    backend ready

--- app/go.mod
+++ app/go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-redis/redis v6.15.9+incompatible
 	github.com/gorilla/mux v1.8.0
 	github.com/sirupsen/logrus v1.6.0
 )
--- app/main.go
+++ app/main.go
@@ -2,17 +2,46 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"net/http"
 	"net/url"
 	"os"
 	"strings"
+	"time"
+	"unicode/utf8"
 
+	"github.com/go-redis/redis"
 	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
 )
 
-var domainName = "cyrilit.tk"
+var domain = os.Getenv("DOMAIN")
 var log = logrus.New()
+var letterRunes = []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+var shortlen = 4
+
+var client = redis.NewClient(&redis.Options{
+	Addr:     os.Getenv("REDIS_ADDR"),
+	Password: os.Getenv("REDIS_PW"),
+	DB:       0,
+})
+
+func trimFirstRune(s string) string {
+	_, i := utf8.DecodeRuneInString(s)
+	return s[i:]
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func RandStringRunes(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
 
 func makeLinkChecks(URL string) error {
 	// if it's not a URL at all
@@ -21,23 +50,34 @@ func makeLinkChecks(URL string) error {
 		return err
 	}
 	// if it's already our URL
-	if strings.HasPrefix(URL, "https://"+domainName) {
+	if strings.HasPrefix(URL, domain) {
 		return fmt.Errorf("It's already short")
 	}
 	return nil
 }
 
-func getLink(w http.ResponseWriter, req *http.Request) {
-	fmt.Println(req.URL)
+func getOriginal(w http.ResponseWriter, req *http.Request) {
+	slink := trimFirstRune(req.URL.Path)
+	val, err := client.Get(slink).Result()
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		log.Error(err)
+		w.WriteHeader(400)
+		fmt.Fprintln(w, "{\"error\":\""+err.Error()+"\"}")
+		return
+	}
+	http.Redirect(w, req, val, 302)
 }
 
 func getShort(w http.ResponseWriter, req *http.Request) {
 	keys, ok := req.URL.Query()["link"]
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 
+	log.Print(keys)
 	if !ok || len(keys[0]) < 1 {
 		log.Error("Param 'link' is missing")
 		w.WriteHeader(400)
-		fmt.Fprintln(w, "Param 'link' is missing")
+		fmt.Fprintln(w, "{\"error\":\"Param 'link' is missing\"}")
 		return
 	}
 
@@ -46,13 +86,37 @@ func getShort(w http.ResponseWriter, req *http.Request) {
 	err := makeLinkChecks(link)
 
 	if err != nil {
-		log.Error(err)
+		log.Errorf("Link checks failed: %v", err)
 		w.WriteHeader(400)
-		fmt.Fprintln(w, err.Error())
+		fmt.Fprintln(w, "{\"error\":\""+err.Error()+"\"}")
+		return
+	}
+	val, err := client.Get(link).Result()
+
+	if err == nil {
+		w.Write([]byte("{\"shortlink\":\"" + domain + val + "\"}"))
 		return
 	}
 
-	w.Write([]byte("https://cyrilit.tk/s/Qwe21"))
+	slink := RandStringRunes(shortlen)
+
+	err = client.Set(slink, link, 0).Err()
+	if err != nil {
+		log.Errorf("Cannot set slink-link kv: %v", err)
+		w.WriteHeader(400)
+		fmt.Fprintln(w, "{\"error\":\""+err.Error()+"\"}")
+		return
+	}
+
+	err = client.Set(link, slink, 0).Err()
+	if err != nil {
+		log.Errorf("Cannot set link-slink kv: %v", err)
+		w.WriteHeader(400)
+		fmt.Fprintln(w, "{\"error\":\""+err.Error()+"\"}")
+		return
+	}
+
+	w.Write([]byte("{\"shortlink\":\"" + domain + slink + "\"}"))
 }
 
 func main() {
@@ -60,10 +124,10 @@ func main() {
 		FullTimestamp: true,
 	}
 	log.SetOutput(os.Stdout)
-
+	log.Printf("Statrting app on %v... Redis: %v", domain, os.Getenv("REDIS_ADDR"))
 	rtr := mux.NewRouter()
-	rtr.HandleFunc("/getlink/{*}", getLink).Methods("GET")
-	rtr.HandleFunc("/getshort", getShort).Methods("GET")
+	rtr.HandleFunc("/{*}", getOriginal).Methods("GET")
+	rtr.HandleFunc("/s/", getShort).Methods("GET")
 	http.Handle("/", rtr)
 
 	http.ListenAndServe(":5000", nil)
